0092bee70127771516d917ce6cff4cc3b035f385
AbdullahAkb <abdullahakb111@gmail.com>
2023-10-25 11:23:40 +0500

    Routes & Utils Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:getx_mvvm/view/splash_screen.dart';
+import 'package:get/get.dart';
+import 'package:getx_mvvm/res/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      // home: const SplashScreen(),
+      getPages: AppRoutes.appRoutes(),
     );
   }
 }
---
+++ lib/utils/utils.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:getx_mvvm/res/colors/app_colors.dart';
+
+class Utils {
+  static void fieldFocusChange(
+      BuildContext context, FocusNode current, FocusNode nextFocus) {
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: AppColors.blackColor,
+    );
+  }
+
+  static snackbar(String title, String message) {
+    Get.snackbar(title, message);
+  }
+}
